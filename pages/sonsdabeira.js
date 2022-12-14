8c1d47c5348921f6ecbde9cf2ca0a1690148260e
José Viana <joseviana@Mac-mini-de-Jose.local>
2022-12-14 12:35:39 -0300

    bsa

--- pages/sonsdabeira.js
+++ pages/sonsdabeira.js
@@ -31,25 +31,6 @@ export async function getStaticProps(request, response) {
     response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
 }
 
-// export default function Home({ sons, infopack }) {
-//     return (
-//         <div className={styles.listadesons}>
-//             <div>
-//             <h1>{infopack.name}</h1>
-//             <h3>{infopack.description}</h3>
-//             <p>Esta coleção possui atualmente <span>{infopack.num_sounds}</span> elementos sonoros.</p>
-//             </div>
-//             <p></p>
-//             <ul>
-//             {sons.map((results) => (
-//                 <li key={results.id}>{results.name}<h6>{results.tags}</h6>
-//                 <iframe frameborder="0" scrolling="no" src={`https://freesound.org/embed/sound/iframe/${results.id}/simple/large/`} width="920" height="245"></iframe></li>
-//             ))}
-//             </ul>
-//         </div>
-//     )
-// }
-
 
 export default function Home({ sons, infopack }) {
 
