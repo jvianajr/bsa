273ceb8b041565102364db52578625628508fcb5
José Viana <joseviana@Mac-mini-de-Jose.local>
2022-12-15 19:58:47 -0300

    bsa

--- pages/sonsdabeira.js
+++ pages/sonsdabeira.js
@@ -7,16 +7,21 @@ import {
     Box,
     AspectRatio,
     Container,
+    Button,
+    Text,
+    Wrap,
+    WrapItem,
+    Center,
   } from '@chakra-ui/react'
 
-
+  
 export async function getStaticProps(request, response) {
     const apiSecret = process.env.CLIENT_SECRET;
     
-    const resPack = await fetch(`https://freesound.org/apiv2/packs/36926?token=${apiSecret}`)
+    const resPack = await fetch(`https://freesound.org/apiv2/packs/36925?token=${apiSecret}`)
     const dataPack = await resPack.json ();
 
-    const res = await fetch(`https://freesound.org/apiv2/packs/36926/sounds?token=${apiSecret}`)
+    const res = await fetch(`https://freesound.org/apiv2/packs/36925/sounds?token=${apiSecret}`)
     const data = await res.json ();
 
     return {
@@ -52,7 +57,15 @@ export default function Home({ sons, infopack }) {
     <AccordionPanel pb={4}>
     <AspectRatio left="0" w="100%" h="0" pos="relative" pb="30%">
     <iframe src={`https://freesound.org/embed/sound/iframe/${results.id}/simple/full_size/`} style={{top: 0, left: 0, width: '100%', height: '100%', position: 'absolute', border: 0,}} allowfullscreen scrolling="no"/>
-      </AspectRatio>
+    </AspectRatio>
+      <Button as="a" target="_blank" mt="3" mr="3" colorScheme='teal' variant='solid' size='xs' href={`https://freesound.org/s/${results.id}/`}>Download</Button> 
+<Wrap>
+{results.tags.map((item, index) => (
+  <WrapItem aligItens="center">
+  <Center mt="3" color="yellow.600" fontSize='xs' key={index}>{item}</Center>
+  </WrapItem>
+    ))}
+    </Wrap>
 </AccordionPanel>
   </AccordionItem>
   ))}
