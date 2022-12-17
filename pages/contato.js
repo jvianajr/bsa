2f9a634ff6e451722613c958f2f54880d25b5638
José Viana <joseviana@Mac-mini-de-Jose.local>
2022-12-17 01:04:59 -0300

    bsa

--- pages/contato.js
+++ pages/contato.js
@@ -1,64 +1,17 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { Container } from '@chakra-ui/react'
-import { Field, Form, Formik } from 'formik';
-
-function FormikExample() {
-  function validateName(value) {
-    let error
-    if (!value) {
-      error = 'Name is required'
-    } else if (value.toLowerCase() !== 'naruto') {
-      error = "Jeez! You're not a fan 😱"
-    }
-    return error
-  }
-}
-
+import { Container, Flex, Text, Box } from '@chakra-ui/react'
 
 export default function Mediacao() {
   return (
     <>
       <Head>
-        <title>Coleções do Banco Sonoro Amazônico</title>
+        <title>BSA _ mediação</title>
         <meta name="keywords" content="inserir palavras chave"></meta>
         <meta name="description" content="inserir descrição da página"></meta>
       </Head>
-      <Container>
-
-        <Formik
-          initialValues={{ name: 'Sasuke' }}
-          onSubmit={(values, actions) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2))
-              actions.setSubmitting(false)
-            }, 1000)
-          }}
-        >
-          {(props) => (
-            <Form>
-              <Field name='name' validate={validateName}>
-                {({ field, form }) => (
-                  <FormControl isInvalid={form.errors.name && form.touched.name}>
-                    <FormLabel>First name</FormLabel>
-                    <Input {...field} placeholder='name' />
-                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <Button
-                mt={4}
-                colorScheme='teal'
-                isLoading={props.isSubmitting}
-                type='submit'
-              >
-                Submit
-              </Button>
-            </Form>
-          )}
-        </Formik>
-        )
-
+      <Container maxW='4xl' height="2xl">
+   contato
       </Container>
     </>
   )
